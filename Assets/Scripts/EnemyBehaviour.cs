4332f17a3e6c476737c2f9657228f508f1586767
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-06-10 14:36:09 -0300

    :robot: AI move-to-core behaviour

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    GameObject _coreReference;
+    NavMeshAgent aiAgent;
+
+
+    void Awake(){
+        aiAgent = this.GetComponent<NavMeshAgent>();
+    }
+
+    void Start()
+    {
+        _coreReference = GameObject.Find("GameManager").GetComponent<GameManager>().Core; // TODO futurely will be changed to a Init method
+        aiAgent.destination = _coreReference.transform.position; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Wave related variables
+    public readonly int totalWaves = 5;
+
+    int _currentWave = 0;
+
+    public int Wave{
+        get { return _currentWave; }
+        set {
+            _currentWave = value;
+            OnWaveNumberChange();
+        }
+    }
+
+    // Core related variables
+    public GameObject _coreGameObject;
+    public GameObject Core{
+        get { return _coreGameObject; }
+    }
+
+    void SetupErrorMessage(string message){
+        throw new System.Exception(string.Format("GameManager not correctly setup: {0}", message));
+    }
+
+    void Awake(){
+        if(_coreGameObject == null) SetupErrorMessage("Core game object not linked");
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void OnWaveNumberChange(){
+
+    }
+}
