e5ebaa68f890cb1055bfd05e99f6d3c750152fb0
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-06-12 17:46:13 -0300

    :rotating_light: Removing lines to stop warnings

--- Assets/Scripts/Units/WallBehaviour.cs
+++ Assets/Scripts/Units/WallBehaviour.cs
@@ -2,16 +2,5 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WallBehaviour : ConstructionBehaviour
-{
-    //// MonoBehaviour methods
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-
-    }
+public class WallBehaviour : ConstructionBehaviour{
 }
