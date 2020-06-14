d0f63c2cebcadea4e03b78af729c1aa53e08d788
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-06-14 15:08:36 -0300

    :sparkles: New UIController for UI canvas

---
+++ Assets/Scripts/UI and IO/UIController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public enum Layout {inGame, gameOver} // TODO - new layouts - , constructionConfirmation, constructionDestruction}
+
+    GameObject _overlayUI;
+    GameObject _popups;
+    GameObject _endGamePopup;
+
+    //// MonoBehaviour
+    void Awake(){
+        _overlayUI = this.transform.Find("OverlayUI").gameObject;
+        _popups = this.transform.Find("Popups").gameObject;
+        _endGamePopup = _popups.transform.Find("EndGame").gameObject;
+
+        ChangeLayout(Layout.inGame);
+    }
+
+    //// Public API
+    public void ChangeLayout(Layout layout){
+        switch(layout){
+            case Layout.inGame:
+                SetActive(_overlayUI, true);
+                SetActive(_endGamePopup, false);
+                break;
+
+            case Layout.gameOver:
+                SetActive(_overlayUI, false);
+                SetActive(_endGamePopup, true);
+                break;
+
+            default:
+                throw new System.Exception(string.Format("Not expected layout {0}", layout));
+        }
+    }
+
+    //// Private methods
+    void SetActive(GameObject gameObject, bool activate){
+        if(gameObject.activeSelf != activate){
+            gameObject.SetActive(activate);
+        }
+    }
+}
