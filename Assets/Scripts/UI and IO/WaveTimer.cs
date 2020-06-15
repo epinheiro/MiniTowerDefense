be01a4ee52f1654f265c9147348e2284ec7bcfae
Eduardo Pinheiro <edu.g.pinheiro@gmail.com>
2020-06-15 15:37:19 -0300

    :art::sparkles: New WaveTimer ui controller

---
+++ Assets/Scripts/UI and IO/WaveTimer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveTimer : MonoBehaviour
+{
+    Slider _bar;
+
+    //// MonoBehaviour
+    void Awake(){
+        _bar = this.GetComponent<Slider>();
+    }
+
+    void Update(){
+        if(_bar.value >= 0){
+            ChangeValueIn(-Time.deltaTime);
+        }else{
+            ChangeValueIn(0);
+        }
+    }
+
+    //// Public API
+    public void SetUp(float max){
+        _bar.maxValue = max;
+        _bar.value = max;
+    }
+
+    // Private methods
+    void ChangeValueIn(float valueToChange){
+        _bar.value += valueToChange;
+    }
+}
